dd3704203391422aa865e8eea09f091175a173b8
Neelpatel00 <patelneel571@gmail.com>
2021-09-04 17:37:56 +0530

    save

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('view engine','ejs');
-app.use(express.static('public'));
+app.use(express.static(__dirname+'/public'));
 
 app.use(session({
     secret: 'abc',
